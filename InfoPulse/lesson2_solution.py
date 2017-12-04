a0444cf9e5048f6e80d6a53266821340bc18312f
rediLgUa <redi.contact@gmail.com>
2017-12-04 12:28:32 +0200

    message

--- InfoPulse/lesson2_solution.py
+++ InfoPulse/lesson2_solution.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 task1string = input("Введите слово(string, строку) = ")
 if task1string.isdecimal():
     print("Вы ввели числовое значение")
@@ -7,3 +6,8 @@ else:
     print("Вы ввели строчное значение")
 print("кол-во пробелов = "+ str(task1string.count(" ")))
 print("кол-во символов '.' = "+ str(task1string.count(".")))
+
+task4 = "Homework"
+print(" "*46 + task4 + " "*46)
+print(task4.upper())
+
