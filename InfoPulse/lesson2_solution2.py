c28c6795e3b1a0f95b756c6f592eee8a80d34c37
rediLgUa <redi.contact@gmail.com>
2017-12-04 14:29:02 +0200

    all

--- InfoPulse/lesson2_solution2.py
+++ InfoPulse/lesson2_solution2.py
@@ -1,7 +1,9 @@
 import math
 import random
+
 # вычисление гипотенузы
 print("gypo = " +str(int(math.sqrt((179**2)+(971**2)))))
+
 # вычисление количества десятков в числе
 i = random.randint(10,99)
 print("двузначное число = " +str(i))
@@ -12,9 +14,18 @@ elif 2 < int(i[0])<5:
     print("в числе "+i[0]+" десятка")
 else:
     print("в числе "+i[0]+" десятков")
+
 # вычисление суммы трех чисел
 i = random.randint(100,999)
 print("трехзначное число = " +str(i))
 i = list(str(i))
 summa = str(int(i[0])+int(i[1])+int(i[2]))
 print("Сумма цифр трехзначного числа = " + summa)
+
+#дано случайное целое число - вывести следующее четное после него
+i = random.randint(0,999)
+print("случайное целое число = " + str(i))
+if i%2==0:
+    print(i+2)
+else: print(i+1)
+
