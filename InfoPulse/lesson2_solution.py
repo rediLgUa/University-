3503f7aeaf4e3de8c31b3cee21021d0bdb82145e
rediLgUa <redi.contact@gmail.com>
2017-12-04 12:01:53 +0200

    Lesson2_Solution file py

---
+++ InfoPulse/lesson2_solution.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+task1string = input("Введите слово(string, строку) = ")
+if task1string.isdecimal():
+    print("Вы ввели числовое значение")
+else:
+    print("Вы ввели строчное значение")
+print("кол-во пробелов = "+ str(task1string.count(" ")))
