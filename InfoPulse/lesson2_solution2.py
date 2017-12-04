2ec993376d9266442ee495ca1707c39110955e32
rediLgUa <redi.contact@gmail.com>
2017-12-04 12:54:00 +0200

    tast2 add

--- InfoPulse/lesson2_solution2.py
+++ InfoPulse/lesson2_solution2.py
@@ -1,2 +1,20 @@
 import math
+import random
+# вычисление гипотенузы
 print("gypo = " +str(int(math.sqrt((179**2)+(971**2)))))
+# вычисление количества десятков в числе
+i = random.randint(10,99)
+print("двузначное число = " +str(i))
+i = list(str(i))
+if int(i[0]) < 2:
+    print("в числе "+i[0]+" десяток")
+elif 2 < int(i[0])<5:
+    print("в числе "+i[0]+" десятка")
+else:
+    print("в числе "+i[0]+" десятков")
+# вычисление суммы трех чисел
+i = random.randint(100,999)
+print("трехзначное число = " +str(i))
+i = list(str(i))
+summa = str(int(i[0])+int(i[1])+int(i[2]))
+print("Сумма цифр трехзначного числа = " + summa)
